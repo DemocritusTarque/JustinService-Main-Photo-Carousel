8bf88d45288c8b1d336663829b69afe007727491
carterwc <cacbcarter68@gmail.com>
2018-12-27 02:50:59 -0500

    updating server get and all db functions

--- Database/index.js
+++ Database/index.js
@@ -14,40 +14,74 @@ connection.connect(error => {
 	console.log('Connected to DB FEC!');
 });
 
-createProduct = function (product, callback) {
-	connection.query(
-		'Insert into product(productName, tid) values(?, ?)',
-		[product.productName, product.tid],
-		(error, productResult) => {
-			if (error) {
-				console.log(error, 'Issue inserting into DB!');
-			} else {
-				console.log(productResult, 'what are results from insert???');
-				var bulkPhotoInsert = product.photos.map(url => {
-					return [productResult.insertId, url];
-				});
-				connection.query(
-					'Insert into images(product_id, urlLink) values ?',
-					[bulkPhotoInsert],
-					(error, results) => {
-						if (error) {
-							console.log(error, 'Issue inserting into DB!');
-						} else {
-							console.log(
-								results,
-								'what are results from insert???'
-							);
-							callback(null, results);
-						}
-					}
-				);
-			}
+// createProduct = function (product, callback) {
+// 	connection.query(
+// 		'Insert into product(productName, tid) values(?, ?)',
+// 		[product.productName, product.tid],
+// 		(error, productResult) => {
+// 			if (error) {
+// 				console.log(error, 'Issue inserting into DB!');
+// 			} else {
+// 				console.log(productResult, 'what are results from insert???');
+// 				var bulkPhotoInsert = product.photos.map(url => {
+// 					return [productResult.insertId, url];
+// 				});
+// 				connection.query(
+// 					'Insert into images(product_id, urlLink) values ?',
+// 					[bulkPhotoInsert],
+// 					(error, results) => {
+// 						if (error) {
+// 							console.log(error, 'Issue inserting into DB!');
+// 						} else {
+// 							console.log(
+// 								results,
+// 								'what are results from insert???'
+// 							);
+// 							callback(null, results);
+// 						}
+// 					}
+// 				);
+// 			}
+// 		}
+// 	);
+// };
+
+getAllPhotos = function (callback) {
+	connection.query('SELECT * from images', (error, images) => {
+		if (error) {
+			console.log('Error getting All Images from DB!', error)
+		} else {
+			console.log(images, 'what are the images we are sending back?????')
+			callback(null, images)
 		}
-	);
+	})
 };
 
-getAllPhotos = function () { };
+getAllProducts = function (callback) {
+	connection.query('SELECT * from product', (error, product) => {
+		if (error) {
+			console.log('Error getting All Products from DB', error);
+		} else {
+			console.log('what are the products we get?!?!', product)
+			callback(null, product)
+		}
+	})
+}
+
+getSpecificProductPhotos = function (id, callback) {
+	connection.query('SELECT * from images WHERE id=(?)', [id], (error, results) => {
+		if (error) {
+			console.log('Error getting Pictures of Specific Product!', error);
+		} else {
+			console.log('what are the pics?!?!', results);
+			callback(null, results);
+		}
+	})
+}
 
 module.exports = {
-	createProduct
+	getAllPhotos,
+	getAllProducts,
+	getSpecificProductPhotos
+	// createProduct
 };
