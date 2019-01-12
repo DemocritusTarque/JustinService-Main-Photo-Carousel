3ee5a776e2d01004adddf8d62d3765dd0f38af44
shakabandai <jus10daniel@gmail.com>
2019-01-12 00:50:19 -0600

    removed extraneous code/formatted files

--- Database/dataGen.js
+++ Database/dataGen.js
@@ -3,43 +3,24 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const faker = require('faker');
-const { pipeline } = require('stream');
-//const streams = require('stream');
 
+for (let i = 1; i < 5; i++) {
+  let filename = path.join(__dirname, `../data${i}.csv`);
+  let output = [];
+  let dbProducts = [];
 
-// pipeline(
-//   fs.createWriteStream('../data.csv')
-//   , (err) => {
-//     if (err) {
-//       console.error('Pipeline failed', err);
-//     } else {
-//       console.log('pipeline succeeded');
-//     }
-//   }
+  while (dbProducts.length < 2500000) {
+    dbProducts.push({ product: faker.commerce.productName(), image: faker.image.imageUrl() })
+  }
 
-// );
+  dbProducts.forEach((d) => {
+    const row = [];
+    row.push(d.product);
+    row.push(d.image);
+    output.push(row.join());
+  });
 
-
-// for(let i = 0; i<10000000; i++){
-//     fs.createWriteStream(`${faker.commerce.productName()},${faker.image.imageUrl}\n`)
-// }
-for(let i = 1; i < 5; i++) {
-let filename = path.join(__dirname, `../data${i}.csv`);
-let output = [];
-let dbProducts = [];
-
-while (dbProducts.length < 2500000) {
-  dbProducts.push({ product: faker.commerce.productName(), image: faker.image.imageUrl() })
-}
-
-dbProducts.forEach((d) => {
-  const row = [];
-  row.push(d.product);
-  row.push(d.image);
-  output.push(row.join());
-});
-
-fs.writeFileSync(filename, output.join(os.EOL));
+  fs.writeFileSync(filename, output.join(os.EOL));
 }
 // goal is to create an fs write stream, utilizing piping/data chunking, 
 // to get to quickly hit 10M in the CSV w/o hitting stack heap
